67c1da281ccddebe7ec3d67f8104aad13868194f
mrmoha7 <maxamedcabdigare5@gmail.com>
2023-08-22 20:28:42 +0300

    finished project

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+    class "com.android.tools.build:gradle:4.1.3"
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url 'https://jitpack.io' }
+    }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
+}
